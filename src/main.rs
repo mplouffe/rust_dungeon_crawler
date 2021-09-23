e4e00eacb94a4458db5dfabb7b8919721234919b
mplouffe <matheuplouffe@gmail.com>
2021-09-22 19:42:13 -0500

    Dungeon Crawler project from Hands-on Rust book. Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+mod map;
+
+use bracket_lib::prelude::*;
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1,1, "Hello, Bracket Terminal!");
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Dungeon Crawler")
+        .build()?;
+    
+    main_loop(context, State {})
+}
