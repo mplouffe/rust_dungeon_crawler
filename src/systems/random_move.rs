fe240931e2bd914d015e2e56bd684e85a3f75e61
mplouffe <matheuplouffe@gmail.com>
2021-09-26 01:21:46 -0500

    Added intent messages and centralized the movement system.

--- src/systems/random_move.rs
+++ src/systems/random_move.rs
@@ -5,13 +5,13 @@ use crate::prelude::*;
 #[read_component(MovingRandomly)]
 pub fn random_move(
     ecs: &mut SubWorld,
-    #[resource] map: &Map
+    commands: &mut CommandBuffer
 )
 {
-    let mut movers = <(&mut Point, &MovingRandomly)>::query();
+    let mut movers = <(Entity, &Point, &MovingRandomly)>::query();
     movers
-        .iter_mut(ecs)
-        .for_each(|(pos, _)| {
+        .iter(ecs)
+        .for_each(|(entity, pos, _)| {
             let mut rng = RandomNumberGenerator::new();
             let destination = match rng.range(0, 4) {
                 0 => Point::new(-1, 0),
@@ -20,9 +20,8 @@ pub fn random_move(
                 _ => Point::new(0, 1),
             } + *pos;
 
-            if map.can_enter_tile(destination) {
-                *pos = destination;
-            }
+            commands
+                .push(((), WantsToMove{ entity: *entity, destination }));
         }
     );
 }
