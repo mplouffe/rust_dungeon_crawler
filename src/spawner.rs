b90a5ad492e7aaca5ae8426935fddf83f92b64f3
mplouffe <matheuplouffe@gmail.com>
2021-09-26 21:02:57 -0500

    Added victory and game over conditions to game.

--- src/spawner.rs
+++ src/spawner.rs
@@ -9,7 +9,22 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
                 color: ColorPair::new(WHITE, BLACK),
                 glyph: to_cp437('@')
             },
-            Health { current: 20, max: 20 }
+            Health { current: 10, max: 10 }
+        )
+    );
+}
+
+pub fn spawn_amulet_of_yala(ecs: &mut World, pos: Point) {
+    ecs.push(
+        (
+            Item, 
+            AmuletOfYala,
+            pos,
+            Render {
+                color: ColorPair::new(WHITE, BLACK),
+                glyph: to_cp437('|')
+            },
+            Name("Amulet of Yala".to_string())
         )
     );
 }
@@ -32,7 +47,7 @@ pub fn spawn_monster(
                 color: ColorPair::new(WHITE, BLACK),
                 glyph,
             },
-            MovingRandomly{},
+            ChasingPlayer {},
             Health{ current: hp, max: hp },
             Name(name)
         )
