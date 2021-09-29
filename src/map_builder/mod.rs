545e388fa25c7eb9cdaad41be5b2d8b4fa9012a7
mplouffe <matheuplouffe@gmail.com>
2021-09-28 23:07:37 -0500

    Added Drunkard's Walk map builder and ability to add prefabs to maps.

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -1,13 +1,14 @@
 use crate::prelude::*;
 use automata::CellularAutomataArchitect;
+use drunkard::DrunkardsWalkArchitect;
 use empty::EmptyArchitect;
+use prefab::apply_prefab;
 use rooms::RoomsArchitect;
 
-
-const NUM_ROOMS: usize = 20;
-
 mod automata;
+mod drunkard;
 mod empty;
+mod prefab;
 mod rooms;
 
 trait MapArchitect {
@@ -51,72 +52,14 @@ impl MapBuilder {
 
 impl MapBuilder {
     pub fn new(rng : &mut RandomNumberGenerator) -> Self {
-        let mut architect = CellularAutomataArchitect {};
-        architect.new(rng)
-    }
-
-    fn build_random_rooms(&mut self, rng : &mut RandomNumberGenerator) {
-        while self.rooms.len() < NUM_ROOMS {
-            let room = Rect::with_size(
-                rng.range(1, SCREEN_WIDTH - 10),
-                rng.range(1, SCREEN_HEIGHT - 10),
-                rng.range(2, 10),
-                rng.range(2, 10), 
-            );
-
-            let mut overlap = false;
-            for r in self.rooms.iter() {
-                if r.intersect(&room) {
-                    overlap = true;
-                }
-            }
-            if !overlap {
-                room.for_each(|p| {
-                    if p.x > 0 && p.x < SCREEN_WIDTH && p.y > 0 && p.y < SCREEN_HEIGHT
-                    {
-                        let idx = map_idx(p.x, p.y);
-                        self.map.tiles[idx] = TileType::Floor;
-                    }
-                });
-                self.rooms.push(room)
-            }
-        }
-    }
-
-    fn apply_vertial_tunnel(&mut self, y1 : i32, y2 : i32, x : i32) {
-        use std::cmp::{min, max};
-        for y in min(y1, y2)..= max(y1, y2) {
-            if let Some(idx) = self.map.try_idx(Point::new(x, y)) {
-                self.map.tiles[idx as usize] = TileType::Floor;
-            }
-        }
-    }
-
-    fn apply_horizontal_tunnel(&mut self, x1 : i32, x2 : i32, y : i32) {
-        use std::cmp::{min, max};
-        for x in min(x1, x2)..= max(x1, x2) {
-            if let Some(idx) = self.map.try_idx(Point::new(x, y)) {
-                self.map.tiles[idx as usize] = TileType::Floor;
-            }
-        }
-    }
-
-    fn build_corridors(&mut self, rng : &mut RandomNumberGenerator) {
-        let mut rooms = self.rooms.clone();
-        rooms.sort_by(|a,b| a.center().x.cmp(&b.center().x));
-
-        for (i, room) in rooms.iter().enumerate().skip(1) {
-            let prev = rooms[i-1].center();
-            let new = room.center();
-
-            if rng.range(0, 2) == 1 {
-                self.apply_horizontal_tunnel(prev.x, new.x, prev.y);
-                self.apply_vertial_tunnel(prev.y, new.y, new.x);
-            } else {
-                self.apply_vertial_tunnel(prev.y, new.y, prev.x);
-                self.apply_horizontal_tunnel(prev.x, new.x, new.y);
-            }
-        }
+        let mut architect : Box<dyn MapArchitect> = match rng.range(0, 3) {
+            0 => Box::new(DrunkardsWalkArchitect {}),
+            1 => Box::new(RoomsArchitect {}),
+            _ => Box::new(CellularAutomataArchitect{}),
+        };
+        let mut mb = architect.new(rng);
+        apply_prefab(&mut mb, rng);
+        mb
     }
 
     fn spawn_monsters(
--- src/map_builder/rooms.rs
+++ src/map_builder/rooms.rs
@@ -1,6 +1,8 @@
 use crate::prelude::*;
 use super::MapArchitect;
 
+const NUM_ROOMS: usize = 20;
+
 pub struct RoomsArchitect {}
 
 impl MapArchitect for RoomsArchitect {
@@ -14,8 +16,8 @@ impl MapArchitect for RoomsArchitect {
         };
 
         mb.fill(TileType::Wall);
-        mb.build_random_rooms(rng);
-        mb.build_corridors(rng);
+        self.build_random_rooms(&mut mb, rng);
+        self.build_corridors(&mut mb, rng);
         mb.player_start = mb.rooms[0].center();
         mb.amulet_start = mb.find_most_distant();
         for room in mb.rooms.iter().skip(1) {
@@ -23,5 +25,71 @@ impl MapArchitect for RoomsArchitect {
         }
         
         mb
+    }
+}
+
+impl RoomsArchitect {
+    fn build_random_rooms(&self, mb: &mut MapBuilder, rng : &mut RandomNumberGenerator) {
+        while mb.rooms.len() < NUM_ROOMS {
+            let room = Rect::with_size(
+                rng.range(1, SCREEN_WIDTH - 10),
+                rng.range(1, SCREEN_HEIGHT - 10),
+                rng.range(2, 10),
+                rng.range(2, 10), 
+            );
+
+            let mut overlap = false;
+            for r in mb.rooms.iter() {
+                if r.intersect(&room) {
+                    overlap = true;
+                }
+            }
+            if !overlap {
+                room.for_each(|p| {
+                    if p.x > 0 && p.x < SCREEN_WIDTH && p.y > 0 && p.y < SCREEN_HEIGHT
+                    {
+                        let idx = map_idx(p.x, p.y);
+                        mb.map.tiles[idx] = TileType::Floor;
+                    }
+                });
+                mb.rooms.push(room)
+            }
+        }
+    }
+
+    fn apply_vertical_tunnel(&self, mb: &mut MapBuilder, y1 : i32, y2 : i32, x : i32) {
+        use std::cmp::{min, max};
+        for y in min(y1, y2)..= max(y1, y2) {
+            if let Some(idx) = mb.map.try_idx(Point::new(x, y)) {
+                mb.map.tiles[idx as usize] = TileType::Floor;
+            }
+        }
+    }
+
+    fn apply_horizontal_tunnel(&self, mb: &mut MapBuilder, x1 : i32, x2 : i32, y : i32) {
+        use std::cmp::{min, max};
+        for x in min(x1, x2)..= max(x1, x2) {
+            if let Some(idx) = mb.map.try_idx(Point::new(x, y)) {
+                mb.map.tiles[idx as usize] = TileType::Floor;
+            }
+        }
+    }
+
+    fn build_corridors(&self, mb: &mut MapBuilder, rng : &mut RandomNumberGenerator) {
+        let mut rooms = mb.rooms.clone();
+        rooms.sort_by(|a,b| a.center().x.cmp(&b.center().x));
+
+        for (i, room) in rooms.iter().enumerate().skip(1) {
+            let prev = rooms[i-1].center();
+            let new = room.center();
+
+            if rng.range(0, 2) == 1 {
+                self.apply_horizontal_tunnel(mb, prev.x, new.x, prev.y);
+                self.apply_vertical_tunnel(mb, prev.y, new.y, new.x);
+            } else {
+                self.apply_vertical_tunnel(mb, prev.y, new.y, prev.x);
+                self.apply_horizontal_tunnel(mb, prev.x, new.x, new.y);
+            }
+        }
     }
 }
