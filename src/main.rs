8ddf688041841228fd14f12c28a528591de13e6a
mplouffe <matheuplouffe@gmail.com>
2021-09-29 20:18:10 -0500

    Added item system with map that reveals level and healing potion. Spawning code needs work.

--- src/main.rs
+++ src/main.rs
@@ -40,11 +40,11 @@ impl State {
         let mut resources = Resources::default();
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
-        spawn_player(&mut ecs, map_builder.player_start);
+        spawn_player(&mut ecs, map_builder.player_start); 
         spawn_amulet_of_yala(&mut ecs, map_builder.amulet_start);
         map_builder.monster_spawns
             .iter()
-            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, *pos));
+            .for_each(|pos| spawn_entity(&mut ecs, &mut rng, *pos));
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
         resources.insert(TurnState::AwaitingInput);
@@ -103,7 +103,7 @@ impl State {
         spawn_amulet_of_yala(&mut self.ecs, map_builder.amulet_start);
         map_builder.monster_spawns
             .iter()
-            .for_each(|pos| spawn_monster(&mut self.ecs, &mut rng, *pos));
+            .for_each(|pos| spawn_entity(&mut self.ecs, &mut rng, *pos));
         self.resources.insert(map_builder.map);
         self.resources.insert(Camera::new(map_builder.player_start));
         self.resources.insert(TurnState::AwaitingInput);
